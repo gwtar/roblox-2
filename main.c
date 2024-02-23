7dded62b3fbf1ff7e28731a3f58da4386854b823
gwtar <notkoishi@gmail.com>
2024-02-23 21:49:33 +0800

    v0.1

--- main.c
+++ main.c
@@ -3,7 +3,22 @@
 
 int main()
 {
-    printf("Hello World\n");
+    const int screenWidth = 700;
+    const int screenHeight = 700;
+    const char *title = "Roblox 2 - v0.1";
 
+    InitWindow(screenWidth, screenHeight, title);
+
+    while(!WindowShouldClose())
+    {
+        BeginDrawing();
+
+        DrawRectangle(screenWidth / 2 - 100, screenHeight / 2 - 100, 200, 200, BLUE);
+
+        ClearBackground(BLACK);
+        EndDrawing();
+    }
+
+    CloseWindow();
     return 0;
 }
